0c56a2dfab5ae053118de579fa78750f9e79c022
Jonathan Ballard <asgardschosen@gmail.com>
2020-02-07 13:36:00 -0800

    models.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from config import db, app
-from sqlalchemy.sql import func
-
-
+from models import Dojo, Ninja
 
 
 
@@ -11,25 +9,6 @@ from sqlalchemy.sql import func
 # the db.Model in parentheses tells SQLAlchemy that this class represents a table in our database
 
 
-
-class Dojo(db.Model):	
-    # __tablename__ = "Dojo"    # optional		
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(45))
-    city = db.Column(db.String(45))
-    state = db.Column(db.String(45))
-    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-
-
-class Ninja(db.Model):	
-    # __tablename__ = "User"    # optional		
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(45))
-    last_name = db.Column(db.String(45))
-    dojo = db.Column(db.String(45))
-    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
 
 
---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy.sql import func
+from config import db
+
+
+class Dojo(db.Model):	
+    # __tablename__ = "Dojo"    # optional		
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+    city = db.Column(db.String(45))
+    state = db.Column(db.String(45))
+    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+
+class Ninja(db.Model):	
+    # __tablename__ = "User"    # optional		
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45))
+    last_name = db.Column(db.String(45))
+    dojo = db.Column(db.String(45))
+    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
